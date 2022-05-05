5e28b6a0a208d53d956ea258692100812c725e67
SynStratos <synstratos.dev@gmail.com>
2022-05-05 12:29:14 +0200

    :bookmark: Release 2.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='gregory',
     packages=setuptools.find_packages(),
-    version='2.1.0',
+    version='2.1.1',
     description='Python framework to manage time series structured as one-level dictionaries.',
     long_description=long_description,
     long_description_content_type="text/markdown",
